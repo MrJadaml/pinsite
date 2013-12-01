e0ef58207a2344b1ed407d14c7a16d986df2015f
MrJadaml <mrjadaml@gmail.com>
2013-11-30 17:06:54 -0700

    restart whole project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinsite::Application.config.secret_key_base = 'c80b45a633562b4c113042b2a1457126586a6c750d4ab05c59449841212db4ddb1045c35172b8e40cd6ff72c14ec635c3815fb77d011bc27cb5a9af8409d159d'
